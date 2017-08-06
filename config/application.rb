32c68a0cfb21f029028d811ccfb0381cb368ee3e
Binit Kumar <binit.kumar.hbs@gmail.com>
2017-08-06 07:49:09 -0400

    Production fixes

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module Just4humanity
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
  
-    config.autoload_paths += %w(#{config.root}/app/models/ckeditor)
+
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
--- db/migrate/20170728072754_add_omniauth_fields_to_user.rb
+++ db/migrate/20170728072754_add_omniauth_fields_to_user.rb
@@ -1,7 +1,7 @@
 class AddOmniauthFieldsToUser < ActiveRecord::Migration[5.0]
   def change
-    #add_column :users, :provider, :string
-    #add_column :users, :uid, :string
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
     add_column :users, :oauth_token, :string
     add_column :users, :oauth_expires_at, :datetime
   end
